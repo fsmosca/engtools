ee966bac024c403ccec302d6246c4c19cc63d406
Ferdinand Mosca <fsmosca@users.noreply.github.com>
2025-03-09 15:42:28 +0100

    Update trunnion.py

--- tools/trunnion.py
+++ tools/trunnion.py
@@ -23,59 +23,63 @@ def find_circle_line_intersection(radius, x_value):
     return radius - y1
 
 
-pipe_data = pd.read_excel("./data/pipe.xlsx", engine="openpyxl")
+pipe_data = pd.read_csv("./data/pipedata.csv")
 dn_list = pipe_data['DN'].tolist()
 
 
-st.markdown('''### TCL Calculator''')
-with st.form('form',clear_on_submit=False, enter_to_submit=False):
-    c1, c2 = st.columns(2)
-    with c1:
-        st.selectbox('☀️ Select Pipe DN', options=dn_list, index=12, key='podk')
-        st.number_input('Input Pad or Wear Plate Thickness',
-                        min_value=0.0, max_value=150.0, step=0.1, key='wptk',
-                        help='For a more accurate thickness, please refer to material specification.')
-    with c2:
-        st.selectbox('⭕ Select Trunnion DN', options=dn_list, index=10, key='todk')
-        st.number_input('Input Trunnion Distance from top of plate to Pipe CL',
-                        value=400.0, min_value=50.0, max_value=2000.0, step=0.1, key='dclk')
-    cal = st.form_submit_button('Calculate', type='primary')
+tab1, tab2 = st.tabs(['🌴 TCL', '💍 PipeData'])
 
-if cal:
-    dn = ss.podk
-    od = pipe_data.loc[pipe_data['DN'] == dn, 'OD'].values[0]
-    r = od/2 + st.session_state.wptk
+with tab1:
+    st.markdown('''### TCL Calculator''')
+    with st.form('form',clear_on_submit=False, enter_to_submit=False):
+        c1, c2 = st.columns(2)
+        with c1:
+            st.selectbox('☀️ Select Pipe DN', options=dn_list, index=12, key='podk')
+            st.number_input('Input Pad or Wear Plate Thickness',
+                            min_value=0.0, max_value=150.0, step=0.1, key='wptk',
+                            help='For a more accurate thickness, please refer to material specification.')
+        with c2:
+            st.selectbox('⭕ Select Trunnion DN', options=dn_list, index=10, key='todk')
+            st.number_input('Input Trunnion Distance from top of plate to Pipe CL',
+                            value=400.0, min_value=50.0, max_value=2000.0, step=0.1, key='dclk')
+        cal = st.form_submit_button('Calculate', type='primary')
 
-    tdn = ss.todk
-    tod = pipe_data.loc[pipe_data['DN'] == tdn, 'OD'].values[0]
-    tr = tod/2
-    dcl = ss.dclk
+    if cal:
+        dn = ss.podk
+        od = pipe_data.loc[pipe_data['DN'] == dn, 'OD'].values[0]
+        r = od/2 + st.session_state.wptk
 
-    ss.tlen = ss.dclk
+        tdn = ss.todk
+        tod = pipe_data.loc[pipe_data['DN'] == tdn, 'OD'].values[0]
+        tr = tod/2
+        dcl = ss.dclk
 
-    with st.container(border=True):
-        if tr >= r:
-            st.error('Error, Trunnion OD must be less than Pipe OD.')
-        else:
-            res = find_circle_line_intersection(r, st.session_state.todk/2)
-            tcl = dcl - r + res
-            st.markdown(f'TCL: **{round(tcl, 1)}**')
+        ss.tlen = ss.dclk
 
-st.markdown('''### Pipe Dimensions''')
-st.dataframe(
-    pipe_data,
-    column_config={
-        "DN": st.column_config.Column(
-            pinned=True,
-        ),
-        "NPS": st.column_config.Column(
-            pinned=True,
-        ),
-        "OD": st.column_config.Column(
-            pinned=True,
-        )
-    },
-    hide_index=True,
-    use_container_width=True,
-    height=250
-)
+        with st.container(border=True):
+            if tr >= r:
+                st.error('Error, Trunnion OD must be less than Pipe OD.')
+            else:
+                res = find_circle_line_intersection(r, st.session_state.todk/2)
+                tcl = dcl - r + res
+                st.markdown(f'TCL: **{round(tcl, 1)}**')
+
+with tab2:
+    st.markdown('''### Pipe Dimensions''')
+    st.dataframe(
+        pipe_data,
+        column_config={
+            "DN": st.column_config.Column(
+                pinned=True,
+            ),
+            "NPS": st.column_config.Column(
+                pinned=True,
+            ),
+            "OD": st.column_config.Column(
+                pinned=True,
+            )
+        },
+        hide_index=True,
+        use_container_width=True,
+        # height=250
+    )
